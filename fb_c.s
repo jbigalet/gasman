181f3eeeebb266114e9697acf3a9e56ab75f04c5
Joran Bigalet <joran.bigalet@gmail.com>
2016-04-17 22:00:04 +0200

    more fb infos

--- fb_c.s
+++ fb_c.s
@@ -1,6 +1,5 @@
 .section .data
-hello:
-  .ascii  "Yu!\n"
+
 fbdev:
   .ascii "/dev/fb0"
 
@@ -16,26 +15,60 @@ main:
 
   mov %rax, %rbp # save handle
 
-  subq $168, %rsp
+  subq $500, %rsp
 
   # get screen infos
   mov %rbp, %rdi # fb handle
   mov $16, %rax # ioctl
-  mov $17920, %rsi
+  mov $17920, %rsi # FBIOGET_VSCREENINFO
   mov %rsp, %rdx
   syscall
 
-  mov $format, %rdi
+  # print screen infos
+  mov $screeninfo, %rdi
   mov (%rsp), %rsi
   mov 4(%rsp), %rdx
   mov 24(%rsp), %rcx
   xor %rax, %rax
   call printf
 
-  #exit
-  mov $60, %rax
-  mov $21, %rdi
+  # screensize = x*y*depth/8. store it in -300
+  mov (%rsp), %rsi
+  imul 4(%rsp), %rsi
+  imul 24(%rsp), %rsi
+  shr $3, %rsi
+  mov %rsi, 300(%rsp)
+
+  # print screensize
+  mov $screensize, %rdi
+  xor %rax, %rax
+  call printf
+
+  # get fixed screen infos
+  mov %rbp, %rdi # fb handle
+  mov $16, %rax # ioctl
+  mov $17922, %rsi # FBIOGET_FSCREENINFO
+  lea 80(%rsp), %rdx
   syscall
 
-format:
+  # print line_length
+  mov $linelen, %rdi
+  mov 128(%rsp), %rsi
+  xor %rax, %rax
+  call printf
+
+  # mmap
+  /* mov $9, %rax */
+  /* syscall */
+
+  #exit
+  mov $60, %rax
+  mov $0, %rdi
+  syscall
+
+screeninfo:
   .asciz "screen info: %dx%d @ %d\n"
+screensize:
+  .asciz "screen size: %d\n"
+linelen:
+  .asciz "linelen: %d\n"
